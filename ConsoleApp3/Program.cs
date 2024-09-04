153474e5cc587e38095968b85919779fc54340ba
ebenezer.v.alcontin <ebenezer.v.alcontin@MIS-DEV>
2024-09-04 16:43:50 +0800

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp3.LINQ.Implementation;
+using ConsoleApp3.LINQ.Interface;
+
+public class Program
+{
+    private static readonly IProduct productService = new ProductService();
+    public static void Main(string[] args)
+    {
+        double minimumPrice = 500.00;
+
+        var expensiveProducts = productService.GetProductNamesAbovePrice(minimumPrice);
+
+        Console.WriteLine("Products with price greater than or equal to " + minimumPrice + ":");
+        foreach (var productName in expensiveProducts)
+        {
+            Console.WriteLine(productName);
+        }
+    }
+}
