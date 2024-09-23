fd62af402e3de6090ba76d74b390abba7ecc3d7b
ebenezer.v.alcontin <ebenezer.v.alcontin@MIS-DEV>
2024-09-23 08:49:05 +0800

    Updated

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,16 +4,25 @@ using ConsoleApp3.LINQ.Interface;
 public class Program
 {
     private static readonly IProduct productService = new ProductService();
+
     public static void Main(string[] args)
     {
-        double minimumPrice = 500.00;
-
-        var expensiveProducts = productService.GetProductNamesAbovePrice(minimumPrice);
-
-        Console.WriteLine("Products with price greater than or equal to " + minimumPrice + ":");
-        foreach (var productName in expensiveProducts)
+        try
         {
-            Console.WriteLine(productName);
+            double minimumPrice = 500.00;
+
+            var expensiveProducts = productService.GetProductNamesAbovePrice(minimumPrice);
+
+            Console.WriteLine("Products with price greater than or equal to " + minimumPrice + ":");
+
+            foreach (var productName in expensiveProducts)
+            {
+                Console.WriteLine(productName);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: ${ex.Message}");
         }
     }
 }
